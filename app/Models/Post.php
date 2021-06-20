a2d0ec218f7af3dade262d6ffffffe3385d16341
redlik <ralphr@outlook.com>
2021-06-20 10:02:55 +0100

    Added custom sorting to Post model to sort by 'publication-date'

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,6 +13,15 @@ class Post extends Model
 
     public function author()
     {
-        return $this->belongsTo('User::class');
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /**
+     * @param $query
+     * @return mixed
+     */
+    public function scopeSortByPublished($query)
+    {
+        return $query->orderBy('publication_date', 'DESC')->get();
     }
 }
