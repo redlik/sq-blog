50042e61c82afc61fab11633ddd0063708d93bd2
redlik <ralphr@outlook.com>
2021-06-20 13:04:49 +0100

    Added call to action if no posts by user

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,9 +7,11 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <h4 class="text-gray-600 mb-8">Listed below are all posts created by you so far</h4>
+            <div class="px-6 md:px-0">
+                <h4 class="text-gray-600 mb-8">Listed below are all posts created by you so far</h4>
+            </div>
             @if ($posts->isNotEmpty())
-            <div class="my-6">
+            <div class="my-6 px-6 md:px-0">
                 <a href="{{route('post.create')}}" class="text-indigo-700 underline hover:text-indigo-400">+ Create
                     new post</a>
             </div>
