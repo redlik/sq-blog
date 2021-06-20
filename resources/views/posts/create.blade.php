1ab67983594aeb4f0f9c2681d1fdf0137e1a8314
redlik <ralphr@outlook.com>
2021-06-20 08:25:24 +0100

    Added post action to create post form to save posts in db

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,10 +9,10 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <form method="POST" action="{{ route('register') }}">
+                    <form method="POST" action="{{ route('post.store') }}">
                     @csrf
 
-                    <!-- Post title -->
+                        <!-- Post title -->
                         <div>
                             <x-label for="title" :value="__('Post title')" />
 
