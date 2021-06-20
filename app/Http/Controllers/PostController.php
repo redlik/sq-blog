d7428d852f08df25e7990d073dfd5b6bb604b5dc
redlik <ralphr@outlook.com>
2021-06-20 08:28:02 +0100

    Added store and list by user functions to PostController with extra routes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,17 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
     public function index()
     {
+        $posts = Post::all();
+
+        return view('posts.index', compact('posts'));
+    }
+
+    public function postsByUser()
+    {
+        $posts = Post::where('user_id', Auth::id())->latest()->get();
+
+        return view('dashboard', compact('posts'));
 
     }
 
     public function create()
     {
-        return view('posts/create');
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|unique:posts|max:255',
+            'description' => 'required',
+        ]);
+
+        $post = new Post();
+
+        $post->title = $request->input('title');
+        $post->description = $request->input('description');
+        $post->publication_date = now();
+        $post->user_id = Auth::id();
+
+        $post->save();
+
+        return redirect('/dashboard');
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::prefix('post')->group(function () {
     Route::get('/create', [PostController::class, 'create'])->middleware(['auth'])->name('post.create');
+    Route::post('/create', [PostController::class, 'store'])->middleware(['auth'])->name('post.store');
 });
 
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/dashboard', [PostController::class, 'postsByUser'])->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
