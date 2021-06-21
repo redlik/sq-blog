db98f3660d7e3a7336cedd0be1991699092adeb7
redlik <ralphr@outlook.com>
2021-06-21 08:17:26 +0100

    Added links inside main feed to open individual posts

--- resources/views/posts/all.blade.php
+++ resources/views/posts/all.blade.php
@@ -1,10 +1,14 @@
 <div class="flex justify-between items-center h-16 hover:bg-gray-100 p-4 rounded border-b border-gray-200">
     <div>
-        <div>{{ $post->title }}</div>
+        <div class="hover:underline hover:text-gray-600">
+            <a href="{{ route('post.show', ['id' => $post->id]) }}">
+            {{ $post->title }}
+            </a>
+        </div>
         <div class="text-sm text-gray-500">Created by: {{ $post->author->name }}</div>
 
     </div>
     <div class="text-sm text-gray-500">
-        Posted on: {{ $post->publication_date }}
+        Posted on: {{ date('d-m-Y', strtotime($post->publication_date)) }}
     </div>
 </div>
--- resources/views/posts/empty.blade.php
+++ resources/views/posts/empty.blade.php
@@ -1,3 +1,4 @@
-<h4 class="text-red-600 mb-8">No posts created yet :-(</h4>
-
+<h4 class="text-red-600 font-bold text-2xl mb-8">No posts created yet :-(</h4>
+@auth
 <a href="{{route('post.create')}}" class="text-indigo-700 underline hover:text-indigo-400">+ Create something</a>
+@endauth
