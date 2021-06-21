9a7c20d61c13572597d7a7ac709e9774539b88bc
redlik <ralphr@outlook.com>
2021-06-21 08:15:22 +0100

    Created post test to check if posts show on homepage

---
+++ tests/Unit/PostTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use App\Models\Post;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class PostTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_if_create_form_page_is_not_visible_to_guests()
+    {
+        $response = $this->post('/post/create');
+
+        $response->assertStatus(302);
+    }
+
+    public function test_if_post_displayed_on_homepage()
+    {
+        $user = User::factory()->create();
+
+        $post = Post::create([
+            'title' => 'Lorem Ipsum',
+            'description' => 'Very long description',
+            'publication_date' => now(),
+            'user_id' => $user->id,
+        ]);
+
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+        $response->assertSee($post->title);
+
+    }
+}
